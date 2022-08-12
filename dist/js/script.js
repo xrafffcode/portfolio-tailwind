0179b79d66d38773d24feca828bc820af2237523
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-12 13:44:03 +0700

    update view

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,21 @@
-// Hamburger
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+// Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
@@ -8,14 +24,59 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('hidden');
 });
 
-// Navbar Fixed
-window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
     }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
 }
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwAe-4USldCSVitDITM5-QkZMd_Y_S0nLakSF3h42UdE90jI1nMR-cr8YIT3QOgQwPtnA/exec';
+const form = document.forms['wpu-contact-form'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    //ketika tombol submit belom diklik
+    //tampilkan tombol loading, hilangan tombol kirim
+    btnLoading.classList.toggle('hidden');
+    btnKirim.classList.toggle('hidden');
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form)
+    })
+        .then(response => {
+            // Tampilkan tombol kirim, hialngkan tombol loading
+            btnLoading.classList.toggle('hidden');
+            btnKirim.classList.toggle('hidden');
+            // Tampilkan alert
+            myAlert.classList.toggle('hidden');
+            // Reset form nya
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message));
+});
